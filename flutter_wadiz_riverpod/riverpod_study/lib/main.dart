636194d938c7a3853b964631772d584acc31d665
chaeyeon <chaenjung.99@gmail.com>
2024-06-23 19:21:13 +0900

    provider 읽기

--- flutter_wadiz_riverpod/riverpod_study/lib/main.dart
+++ flutter_wadiz_riverpod/riverpod_study/lib/main.dart
@@ -3,16 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_study/provider/counter_provider.dart';
 
 void main() {
-  final container = ProviderContainer();
-  final counterClass = container.read(counterProvider);
-  print(counterClass.counterValue);
-  counterClass.increment();
-
-  final counter2Class = container.read(counterProvider);
-  print(counter2Class.counterValue);
-  counter2Class.increment();
-
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      //Provider Container를 가지고있다.
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -22,60 +18,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        body: const Center(
+          child: Text('Hello World!'),
+        ),
+        floatingActionButton: Consumer(
+          builder: (context, ref, child) {
+            final counter = ref.read(counterProvider);
+            return FloatingActionButton(
+              onPressed: () {
+                counter.increment();
+              },
+              child: const Icon(
+                Icons.add,
+              ),
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
