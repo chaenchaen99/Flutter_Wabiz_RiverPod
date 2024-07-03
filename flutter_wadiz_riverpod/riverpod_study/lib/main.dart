5a8557fa55ad3dfee7fb4814658df444ac98db04
chaeyeon <chaenjung.99@gmail.com>
2024-07-03 21:24:51 +0900

    streambuilder 실습

--- flutter_wadiz_riverpod/riverpod_study/lib/main.dart
+++ flutter_wadiz_riverpod/riverpod_study/lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_study/future_provider/simple_future_provider.dart';
-import 'package:riverpod_study/provider/counter_cfw.dart';
-import 'package:riverpod_study/provider/counter_consumerwidget.dart';
-import 'package:riverpod_study/provider/counter_provider.dart';
-import 'package:riverpod_study/state_notifier_provider/my_state_notifier_provider.dart';
-import 'package:riverpod_study/state_provider/my_state_provider.dart';
+import 'package:riverpod_study/stream_provider/simple_stream_provider.dart';
 
 void main() {
   runApp(
@@ -33,24 +28,51 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: MyFutureProviderWidget(),
+    return Scaffold(
+      //body: Center(child: MyStreamProviderWidget()),
+      body: Consumer(
+        builder: (BuildContext context, WidgetRef ref, Widget? child) {
+          return StreamBuilder(
+            //StreamBuilder는 주어진 스트림을 구독하고, 스트림의 데이터나 상태가 변경될 때마다 UI를 빌드
+            stream: ref.watch(simpleStreamProvider.future).asStream(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Center(
+                  child: Text("${snapshot.data}"),
+                );
+              }
+              return const Text("로딩중");
+            },
+          );
+        },
+      ),
     );
   }
 }
 
-class MyFutureProviderWidget extends ConsumerWidget {
-  const MyFutureProviderWidget({super.key});
+class MyStreamProviderWidget extends ConsumerWidget {
+  const MyStreamProviderWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final intValue = ref.watch(simpleIntFutureProvider);
-    return intValue.when(data: (data) {
-      return Center(child: Text("$data"));
-    }, error: (error, trace) {
-      return Center(child: Text("$error"));
-    }, loading: () {
-      return const Center(child: Text("로딩중"));
-    });
+    final counter = ref.watch(simpleStreamProvider);
+    return switch (counter) {
+      AsyncData(:final value) => Text("$value"),
+      AsyncError(:final error) => Text("$error"),
+      _ => const Text("로딩중"),
+    };
+    // return counter.when( // counter라는 변수의 상태에 따라 위젯을 렌더링
+    //   data: (data) => Center(
+    //     child: Text("$data"),
+    //   ),
+    //   error: (error, trace) {
+    //     return Center(
+    //       child: Text("$error"),
+    //     );
+    //   },
+    //   loading: () => const Center(
+    //     child: Text("로딩중"),
+    //   ),
+    // );
   }
 }
