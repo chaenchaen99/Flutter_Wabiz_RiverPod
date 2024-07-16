6c10fbb0a7d42566446fd3fc3a1c32615b8e82a3
chaeyeon <chaenjung.99@gmail.com>
2024-07-16 12:21:55 +0900

    카테고리 화면 가로 스크롤 위젯 생성

--- flutter_widiz/lib/views/category/category_page.dart
+++ flutter_widiz/lib/views/category/category_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
 
 class CategoryPage extends StatefulWidget {
   final String categoryId;
@@ -11,6 +14,105 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("카테고리"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              "assets/icons/home_outlined.svg",
+              width: 24,
+              height: 24,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          //최상위 프로젝트
+          SizedBox(
+            height: 204,
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.grey[800],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "최고의 이어폰 | 전문가가 설계한 섬세한 이어폰",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const Gap(12),
+                  const Text(
+                    "전문가가 만든 이어폰 하나둘셋넷다섯",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const Gap(16),
+                  Container(
+                    height: 4,
+                    width: 120,
+                    decoration: const BoxDecoration(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(6, 16, 8, 0),
+            height: 110,
+            decoration: const BoxDecoration(
+              color: Colors.grey,
+            ),
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                      left: 32,
+                    ),
+                    child: IntrinsicWidth(
+                      child: Column(
+                        children: [
+                          const Gap(8),
+                          const CircleAvatar(
+                            radius: 16,
+                          ),
+                          const Gap(12),
+                          const Text("테크"),
+                          const Gap(12),
+                          Container(
+                            height: 6,
+                            color: Colors.black,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
   }
 }
